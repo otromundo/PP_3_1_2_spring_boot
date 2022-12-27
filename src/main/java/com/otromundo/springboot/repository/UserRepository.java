c49707e6c7e2b4d2bd6a8bed9674cff07ad851bf
Aleksei Vakhrin <aavakhrin@gmail.com>
2022-12-27 19:40:43 +0400

    minor fix

--- src/main/java/com/otromundo/springboot/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.otromundo.springboot.repository;
-
-import com.otromundo.springboot.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
